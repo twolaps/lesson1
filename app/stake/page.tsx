eb763e14e99319f72426953e353acb575d4daea1
edison <twolaps@163.com>
2025-11-30 21:28:50 +0800

    tj

--- app/stake/page.tsx
+++ app/stake/page.tsx
@@ -1,23 +1,126 @@
 'use client';
-import { Box, Divider, Typography } from "@mui/material";
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, TextField, Typography } from "@mui/material";
 import { HeadView } from "../components/HeadView";
-import { useAccount, useReadContract } from "wagmi";
+import { useAccount, useBalance, useReadContract, useWaitForTransactionReceipt, useWriteContract } from "wagmi";
 import { stakeAddress } from "@/constants/address";
 import { stakeAbi } from "@/constants/abi/stakeABI";
-import { formatUnits } from "viem";
+import { formatUnits, parseEther } from "viem";
+import { useEffect, useState } from "react";
 
 export default function StakePage() {
-
     const { address: myAddress } = useAccount();
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [amount, setStakeAmount] = useState<string>('');
+    const {data: balanceData, isLoading, isError, refetch: refetchBalance} = useBalance({address: myAddress});
+    const [stakeStatus, setStakeStatus] = useState<string>("");
+    const {writeContractAsync } = useWriteContract();
+    const [txHash, setTxHash] = useState<`0x${string}` | undefined>(undefined);
+    const [btnEnabled, setBtnEnabled] = useState<boolean>(true);
+    
+    const { 
+        isSuccess: isReceiptSuccess, 
+        isError: isReceiptError, 
+        error: receiptError 
+    } = useWaitForTransactionReceipt({ hash: txHash, query: { enabled: !!txHash } });
 
+    
+    let balanceStr: string = '';
+    if (isLoading) {
+        balanceStr = 'Loading...';
+    }
+    else if (isError) {
+        balanceStr = 'Error fetching balance';
+    }
+    else if (!balanceData) {
+        balanceStr = 'No balance data';
+    }
+    else {
+        const balance:string = Number(formatUnits(balanceData.value, balanceData.decimals ?? 18)).toFixed(4);
+        balanceStr = balance;
+    }
 
-    const {data: stakedAmount} = useReadContract({
+    const {data: stakedAmount, refetch: refetchStakedAmount} = useReadContract({
         address: stakeAddress,
         abi: stakeAbi,
         functionName: 'stakingBalance',
         args: [BigInt(0), myAddress!],
         query: { enabled: !!myAddress }
     });
+
+    const onChangeAmount = (event: React.ChangeEvent<HTMLInputElement>)=> {
+        setStakeAmount(String(event.target.value));
+    }
+
+    const onClickStake = ()=> {
+        if (!myAddress) {
+            alert("请先连接钱包");
+            return;
+        }
+
+        if (Number(amount) <= 0) {
+            alert("请输入正确的质押数量");
+            return;
+        }
+
+        try {
+            if (balanceData && parseEther(amount) > balanceData.value) {
+                alert("余额不足");
+                return;
+            }
+        }
+        catch {
+            alert("请输入正确的质押数量");
+            return;
+        }
+
+        setDialogOpen(true);
+    }
+
+    const onClickConfirmStake = async ()=> {
+        setDialogOpen(false);
+        setBtnEnabled(false);
+
+        try {
+            
+            setStakeStatus("质押中...");
+            const tx: `0x${string}` =  await writeContractAsync({
+                address: stakeAddress,
+                abi: stakeAbi,
+                functionName: 'depositETH',
+                value: parseEther(amount),
+            });
+            setTxHash(tx);
+        
+        } catch (e) {
+            setBtnEnabled(true);
+            setStakeStatus("质押失败: " + (e as Error).message);
+            alert("质押失败: " + (e as Error).message);
+        }
+    }
+
+    const onClickCancelStake = ()=> {
+        setDialogOpen(false);
+    }
+
+    useEffect(()=>{
+        if (isReceiptSuccess) {
+            refetchBalance();
+            refetchStakedAmount();
+            setStakeStatus("质押成功");
+            setStakeAmount('');
+            alert("质押成功");
+            setBtnEnabled(true);
+            setTxHash(undefined);
+        }
+        else if (isReceiptError) {
+            console.log("交易失败，原因：" + receiptError?.message);
+            setStakeStatus("质押失败: " + receiptError?.message);
+            alert("质押失败: " + receiptError?.message);
+            setBtnEnabled(true);
+            setTxHash(undefined);
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isReceiptSuccess, isReceiptError, receiptError]);
 
     return (
         <div>
@@ -27,7 +130,29 @@ export default function StakePage() {
             <Typography variant="h5" align="center">Stake ETH to earn tokens</Typography>
             <Box height={60} />
             <Typography variant="h5" align="center">Staked Amount</Typography>
-            <Typography variant="h2" align="center">{stakedAmount ? Number(formatUnits(stakedAmount, 18)).toFixed(2) : "0.00"} ETH</Typography>
+            <Typography variant="h2" align="center">{stakedAmount ? Number(formatUnits(stakedAmount, 18)).toFixed(4) : "0.0000"} ETH</Typography>
+            <Box display="flex" justifyContent="center" mt={4}>
+                <TextField disabled={!btnEnabled} placeholder="0.00" slotProps={{htmlInput: {step: "0.01",min: "0"}}} value={amount} onChange={onChangeAmount} type="number" label="Amount to Stake (ETH)" variant="outlined" sx={{mt: 4, minWidth:500}} />
+                <Typography variant="h5" align="center" sx={{mt: 5, ml:2}}>ETH</Typography>
+            </Box>
+            <Typography variant="h5" align="center" sx={{mt: 5, ml:2}}>available: {balanceStr} ETH</Typography>
+
+            <Button onClick={onClickStake} variant="contained" color="primary" disabled={!btnEnabled} sx={{mt: 4, display: 'block', mx: 'auto',width:300, height:60, fontSize:24}}>
+                Stake Now
+            </Button>
+
+            <Typography variant="h5" align="center" sx={{mt: 5, ml:2}}>{stakeStatus}</Typography>
+
+
+            <Dialog open={dialogOpen} onClose={onClickCancelStake}>
+                <DialogTitle>提示</DialogTitle>
+                <DialogContent>你确定要执行此操作吗？</DialogContent>
+                <DialogActions>
+                    <Button onClick={onClickCancelStake}>取消</Button>
+                    <Button onClick={onClickConfirmStake} autoFocus color="primary">确定</Button>
+                </DialogActions>
+            </Dialog>
+            
         </div>
     );
 }
